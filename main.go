ff86dce6a51e4e88cf13dedceb9244d65705ee8b
Antonio Thomacelli <tonnytg@gmail.com>
2021-06-25 11:53:21 -0300

    feat: created segregation of responsibility for go test

--- main.go
+++ main.go
@@ -11,46 +11,54 @@ import (
 
 // this struct need refact just like you need
 type PersonalInfo struct {
-	Name string
-	Surname string
-	Lastname string
-	Sex string
-	Age string
-	Address string
-	City string
-	Zipcode string
-	County string
-	Countrycode string
-	Tephone string
+	Name        string
+	Surname     string
+	Lastname    string
+	Sex         string
+	Age         string
+	Address     string
+	City        string
+	Zipcode     string
+	County      string
+	CountryCode string
+	Telephone   string
 }
 
-func main() {
-	// Convert string to int
-	// Parse this line passed with first argument
+// GetArguments obtain values of parameters passed for binary LINE FILE
+func GetArguments() error {
 	argLine := os.Args[1]
+	// Get INT to read one line of file CSV
 	numLine, _ := strconv.ParseInt(argLine, 0, 64)
-	fmt.Println("I will check line:", numLine)
 
-	// Open file if exist
-	// This is the second argument after number of line
+	// Get PATH of file
 	argFile := os.Args[2]
-	fmt.Println("Start check, wait...")
+
+	ConvertCSVtoStruct(argFile, numLine)
+
+	return nil
+}
+
+// ConvertCSVtoStruct get information of file and convert to Struct for Go can understand
+func ConvertCSVtoStruct(argFile string, numLine int64) error {
 	csvFile, err := os.Open(argFile)
 	if err != nil {
-		fmt.Println("Fail to open file", err)
+		fmt.Errorf("error to open file: %v", err)
 	}
-
-	// Close process after read .csv
 	defer csvFile.Close()
-	// read all lines with ReadAll()
+
 	csvLines, err := csv.NewReader(csvFile).ReadAll()
 	if err != nil {
-		fmt.Println("Err to read file", err)
+		fmt.Errorf("error to read file: %v", err)
 	}
+	ShowValues(csvLines, numLine)
+	return nil
+}
+
+// ShowValues show values of conversion
+func ShowValues(csvLines [][]string, numLine int64) error {
 	// Use ; for delimiter information
-	var b [14]string
+	var b [15]string
 	for i, j := range strings.Split(csvLines[numLine][0], ";") {
-		// fmt.Println(i)
 		b[i] = j
 	}
 	// Get 11 positions of .csv
@@ -58,11 +66,16 @@ func main() {
 		b[3], b[4], b[5], b[6],
 		b[7], b[8], b[9], b[10],
 	}
+	fmt.Println(s)
 	v := reflect.ValueOf(s)
 	typeOfS := v.Type()
-	// imprime cada valor coletado
+	// print each value
 	for i := 0; i < v.NumField(); i++ {
 		fmt.Printf("%s: %v\n", typeOfS.Field(i).Name, v.Field(i).Interface())
 	}
+	return nil
 }
 
+func main() {
+	GetArguments()
+}
